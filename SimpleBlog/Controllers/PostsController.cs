c9f1698dbdbe2b0b0fc5a8e3cfe3881df0b290b1
Anmol Desai <ades597@aucklanduni.ac.nz>
2016-09-01 17:47:17 +1200

    add POsts ctroller for home

---
+++ SimpleBlog/Controllers/PostsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Controllers
+{
+    public class PostsController : Controller
+    {   
+        // needs to be public. Returns ActionResult.  Controller library has methods that construct ActionResult.
+        public ActionResult Index()
+        {
+            return Content("hello world");
+        }
+    }
+}
