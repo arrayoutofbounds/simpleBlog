c3f515ca448c66dcb134e3a8cd4a6463cbcf6b4d
Anmol Desai <ades597@aucklanduni.ac.nz>
2016-09-05 23:05:58 +1200

    view models. Validated information. Validated input fields.  Use form helpers and html validators

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -17,7 +17,17 @@ namespace SimpleBlog.Controllers
         [HttpPost]
         public ActionResult Login(ViewModels.AuthLogin form)
         {
-            return Content("hi " +  form.Username);
+
+            if (!ModelState.IsValid) // if view model does not pass checks for annotations 
+                return View(form);
+
+            if(form.Username != "rainbow") // usually a database check 
+            {
+                ModelState.AddModelError("Username", "Username not valid");
+                return View(form);
+            }
+
+            return Content("Valid");
         }
     }
 }
